bb892dceaf6b97f8847c5b068b727ec5c9171c04
SOMON SOUM <somon.soum9@gmail.com>
2023-01-16 06:55:24 +0700

    change connection database with uri

--- src/config/sequelize.ts
+++ src/config/sequelize.ts
@@ -2,13 +2,8 @@ import { Sequelize } from 'sequelize'
 import { config } from 'dotenv'
 
 config()
-const sequelize = new Sequelize(
-  'project_apollo_server',
-  'godital',
-  'Anna@128@04$2023',
-  {
-    host: `${process.env.HOST}`,
-    dialect: 'mysql',
-  },
-)
+const sequelize = new Sequelize(`${process.env.MYSQL_DEFAULT}`, {
+  host: `${process.env.HOST}`,
+  dialect: 'mysql',
+})
 export default sequelize
