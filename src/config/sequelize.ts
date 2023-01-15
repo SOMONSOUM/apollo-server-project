780541e87fdf544ccd9dbe6a8a3d0c4f073de4a7
SOMON SOUM <somon.soum9@gmail.com>
2023-01-16 05:04:06 +0700

    change databases

--- src/config/sequelize.ts
+++ src/config/sequelize.ts
@@ -4,8 +4,8 @@ import { config } from 'dotenv'
 config()
 const sequelize = new Sequelize(
   'project_apollo_server',
-  'sammy',
-  'Anna@128#04$2023',
+  'godital',
+  'Anna@128@04$2023',
   {
     host: `${process.env.HOST}`,
     dialect: 'mysql',
